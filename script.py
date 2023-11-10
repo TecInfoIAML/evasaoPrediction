d1fc067444f4c58c34016841a41cfd39d861205e
TecInfoIAML <144251350+TecInfoIAML@users.noreply.github.com>
2023-11-10 05:10:52 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,24 @@
+import joblib  # Para modelos scikit-learn
+
+# Carregar o modelo treinado
+model = joblib.load("seu_modelo.pkl")  # Para modelos scikit-learn
+
+# Função para fazer previsões
+def fazer_previsao(dados):
+    # Realizar pré-processamento dos dados, se necessário
+    # Fazer previsões com o modelo
+    resultado = model.predict(dados)
+    return resultado
+    
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/prever_evasao', methods=['POST'])
+def prever_evasao():
+    dados = request.json  # Recebe os dados do formulário como JSON
+    resultado = fazer_previsao(dados)
+    return jsonify({"previsao": resultado.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
